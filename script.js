9629f13a986c5b2c23047b28a0fd0848da5f6229
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-08 21:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+// Get all elements with data-ns-test="prices" attribute
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Initialize total price to zero
+let totalPrice = 0;
+
+// Loop through the price elements and sum their values
+priceElements.forEach((priceElement) => {
+  totalPrice += parseFloat(priceElement.textContent);
+});
+
+// Get the grand total cell
+const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
+
+// Update the grand total cell with the calculated total
+grandTotalCell.textContent = totalPrice.toFixed(2); // Assuming you want to display the total with two decimal places
